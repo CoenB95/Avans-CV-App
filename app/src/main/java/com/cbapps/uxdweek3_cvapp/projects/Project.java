4b1fe81ec9ce3eb7283782165cf25cb8b0535033
Coen <boelhouwers.coen@gmail.com>
2017-05-13 14:20:42 +0200

    Added new projects.

--- app/src/main/java/com/cbapps/uxdweek3_cvapp/projects/Project.java
+++ app/src/main/java/com/cbapps/uxdweek3_cvapp/projects/Project.java
@@ -14,15 +14,32 @@ public class Project implements Serializable {
 
 	public static final Project[] ALL_PROJECTS = {
 			new Project("CV App", R.mipmap.ic_launcher, 2017,
-					"Mijn persoonlijke informatie gepresenteerd [Android] (Avans TI 1.4)",
+					"Mijn CV [Android] (Avans TI 1.4)",
 					"Voor de 4e periode in het eerste leerjaar van de opleiding TI op Avans Breda, " +
 							"diende er een applicatie gemaakt te worden om jezelf te promoten; een " +
 							"CV app. Zie hier voor U het resultaat daarvan."),
 			new Project("Festivalplanner", R.mipmap.photo_festivalplanner, 2017,
 					"Een Festival Simulator [Java] (Avans TI 1.3)",
-					"Voor de 4e periode in het eerste leerjaar van de opleiding TI op Avans Breda, " +
-							"diende er een applicatie gemaakt te worden om jezelf te promoten; een " +
-							"CV app. Zie hier voor U het resultaat daarvan."),
+					"Het derde project in opdracht van Avans betrof een desktop simulatie " +
+							"programma voor het plannen van festivals. Vooral het simulator " +
+							"gedeeldte is naar mijn mening vrij mooi geworden en bijna bug-vrij " +
+							"geworden. Het was een leuk project, met een even zo gezellige " +
+							"groep."),
+			new Project("Boe(m)Bot", R.mipmap.photo_boe_bot, 2017,
+					"Automatisch Geleid Voertuig [Java] (Avans TI 1.2)",
+					"In periode twee van de opleiding werden de opdrachten al wat interessanter. " +
+							"Deze keer moesten we een Board-of-Education-Bot laten rondrijden. " +
+							"Het interessante voor mij eraan was dat ik nog nooit eerder met " +
+							"Java iets fysieks had aangestuurd, ik dacht eigenlijk dat dat niet " +
+							"kon. Dit project heeft daarom mijn ogen meer geopend, al verloopt " +
+							"thuis ermee oefenen moeilijk als je die hardware niet hebt."),
+			new Project("Internet Weerstation", R.mipmap.photo_iwapp, 2017,
+					"Java basics leren [Java] (Avans TI 1.1)",
+					"Het allereerste project in de opleiding TI op Avans Breda. Dit project " +
+							"was bedoeld ter introductie voor mensen met minder ervaring, en " +
+							"is daardoor niet zo spannend. Wel hadden we er al geavanceerde " +
+							"elementen in gestopt zoals gebruik van lambda's, dat op zich " +
+							"liet wachten tot de derde periode."),
 			new Project("Roadplacer", R.mipmap.photo_roadplacer, 2017,
 					"Een Mini Metro remake hobbyproject [JavaFX]",
 					"Dit hobby-project is geïnspireerd door het spel 'Mini Metro' en was mijn " +
